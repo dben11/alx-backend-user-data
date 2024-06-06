33ba2e0d104ef442472fecb83c6b1b7415911a35
dben <sam.dben@gmail.com>
2024-06-05 17:22:55 -0700

    basic auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -24,13 +24,6 @@ elif AUTH_TYPE == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 
-# check the AUTH_TYPE if equal to basic_auth
-if AUTH_TYPE == 'basic_auth':
-    from api.v1.auth.basic_auth import BasicAuth
-    auth = BasicAuth()
-else:
-    auth = Auth()
-
 
 @app.before_request
 def before_request():
