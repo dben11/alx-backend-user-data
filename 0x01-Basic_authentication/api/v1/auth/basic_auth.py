68cc910ab42b7df2f01c769436576491dd2db5bf
dben <sam.dben@gmail.com>
2024-06-05 18:39:23 -0700

    update git push

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,7 +2,28 @@
 """Basic auth class"""
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
-    """__summary__"""
+    """_summary_
+    """
+
+    def extract_base64_authorization_header(
+	    self, authorization_header, str) -> str:
+	"""_summary_
+
+	Args:
+		str: _description_
+
+	Returns:
+		str:_description_
+	"""
+	if authorization_header is None:
+	    return None
+	if not isinstance(authorization_header, str):
+	    return None
+	if not authorization_header.startswith("Basics"):
+	    return None
+	else:
+	    return authorization_header.split(' ')[1]
